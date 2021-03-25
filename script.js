45f33ef8ff26a403abcdc6bdce052dbb68872f6f
Michael Cordeiro <62896592+michaelcordeiro@users.noreply.github.com>
2021-03-25 00:42:47 -0300

    new commit
    add Functionalities

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
 
     for(let i=0;i<etapa.numeros;i++) {
         if(i === 0) {
@@ -71,10 +74,15 @@ function clicou(n) {
     }
 }
 function branco() {
-    alert("Clicou em BRANCO!");
+    numero = '';
+    votoBranco = true;
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
 }
 function corrige() {
-    alert("Clicou em CORRIGE!");
+    comecarEtapa();
 }
 function confirma() {
     alert("Clicou em CONFIRMA!");
